4d99bb9b6b382482e0c84763165d8e45db1c1d46
Chris Luo <chris.ying.luo@gmail.com>
2019-12-24 09:56:09 -0800

    restart activity after crash

--- acra-core/src/main/java/org/acra/util/ProcessFinisher.java
+++ acra-core/src/main/java/org/acra/util/ProcessFinisher.java
@@ -18,11 +18,15 @@ package org.acra.util;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Process;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import org.acra.ACRA;
 import org.acra.builder.LastActivityManager;
@@ -54,13 +58,15 @@ public final class ProcessFinisher {
 
     public void endApplication() {
         stopServices();
+        doRestart(context);
         killProcessAndExit();
     }
 
     public void finishLastActivity(@Nullable Thread uncaughtExceptionThread) {
-        if (ACRA.DEV_LOGGING) ACRA.log.d(LOG_TAG, "Finishing activities prior to killing the Process");
+        if (ACRA.DEV_LOGGING)
+            ACRA.log.d(LOG_TAG, "Finishing activities prior to killing the Process");
         boolean wait = false;
-        for(Activity activity : lastActivityManager.getLastActivities()) {
+        for (Activity activity : lastActivityManager.getLastActivities()) {
             final boolean isMainThread = uncaughtExceptionThread == activity.getMainLooper().getThread();
             final Runnable finisher = () -> {
                 activity.finish();
@@ -93,7 +99,8 @@ public final class ProcessFinisher {
                             intent.setComponent(serviceInfo.service);
                             context.stopService(intent);
                         } catch (SecurityException e) {
-                            if (ACRA.DEV_LOGGING) ACRA.log.d(LOG_TAG, "Unable to stop Service " + serviceInfo.service.getClassName() + ". Permission denied");
+                            if (ACRA.DEV_LOGGING)
+                                ACRA.log.d(LOG_TAG, "Unable to stop Service " + serviceInfo.service.getClassName() + ". Permission denied");
                         }
                     }
                 }
@@ -106,5 +113,44 @@ public final class ProcessFinisher {
     private void killProcessAndExit() {
         Process.killProcess(Process.myPid());
         System.exit(10);
+    }
+
+    private void doRestart(Context c) {
+        try {
+            //check if the context is given
+            if (c != null) {
+                //fetch the packagemanager so we can get the default launch activity
+                // (you can replace this intent with any other activity if you want
+                PackageManager pm = c.getPackageManager();
+                //check if we got the PackageManager
+                if (pm != null) {
+                    //create the intent with the default start activity for your application
+                    Intent mStartActivity = pm.getLaunchIntentForPackage(
+                            c.getPackageName()
+                    );
+                    if (mStartActivity != null) {
+                        mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        //create a pending intent so the application is restarted after System.exit(0) was called.
+                        // We use an AlarmManager to call this intent in 100ms
+                        int mPendingIntentId = 223344;
+                        PendingIntent mPendingIntent = PendingIntent
+                                .getActivity(c, mPendingIntentId, mStartActivity,
+                                        PendingIntent.FLAG_CANCEL_CURRENT);
+                        AlarmManager mgr = (AlarmManager) c.getSystemService(Context.ALARM_SERVICE);
+                        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+                        //kill the application
+                        // System.exit(0);
+                    } else {
+                        Log.e(LOG_TAG, "Was not able to restart application, mStartActivity null");
+                    }
+                } else {
+                    Log.e(LOG_TAG, "Was not able to restart application, PM null");
+                }
+            } else {
+                Log.e(LOG_TAG, "Was not able to restart application, Context null");
+            }
+        } catch (Exception ex) {
+            Log.e(LOG_TAG, "Was not able to restart application");
+        }
     }
 }
